50b83da8dc633255081d62b07df51d26b617f9a9
Jordan <96508953+Voltorik@users.noreply.github.com>
2023-03-10 21:33:55 +0000

    Changed code spacing

--- config/Database.php
+++ config/Database.php
@@ -1,37 +1,36 @@
 <?php 
 class Database {
-    // DB Params
-    private $conn;
-    private $host;
-    private $port;
-    private $db_name;
-    private $username;
-    private $password;
-    
-    public function __construct() {
-        $this->username = getenv('USERNAME');
-        $this->password = getenv('PASSWORD');
-        $this->db_name = getenv('DBNAME');
-        $this->host = getenv('HOST');
-        $this->port = getenv('PORT');
-    }
+  // DB Params
+  private $conn;
+  private $host;
+  private $port;
+  private $db_name;
+  private $username;
+  private $password;
+  
+  public function __construct() {
+      $this->username = getenv('USERNAME');
+      $this->password = getenv('PASSWORD');
+      $this->db_name = getenv('DBNAME');
+      $this->host = getenv('HOST');
+      $this->port = getenv('PORT');
+  }
 
-    // DB Connect
-    public function connect() {
-        // connection already exists, return it
-        if ($this->conn) {
-          return $this->conn;
-        } else {
-  
-          $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name};";
-  
-          try { 
-            $this->conn = new PDO($dsn, $this->username, $this->password);
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $this->conn;
-          } catch(PDOException $e) {
-            echo 'Connection Error: ' . $e->getMessage();
-          }
-        }
+  // DB Connect
+  public function connect() {
+    // connection already exists, return it
+    if ($this->conn) {
+      return $this->conn;
+    } else {
+      $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name};";
+
+      try { 
+        $this->conn = new PDO($dsn, $this->username, $this->password);
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $this->conn;
+      } catch(PDOException $e) {
+        echo 'Connection Error: ' . $e->getMessage();
       }
+    }
+  }
 }
